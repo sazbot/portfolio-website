19ea9f2277416da4e087444266214ffbb24b1547
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-08-31 17:30:36 +0100

    complete render-project script

--- render-project.js
+++ render-project.js
@@ -1,5 +1,22 @@
+const projectOne = document.querySelector(".project-one");
+const projectTwo = document.querySelector(".project-two");
+const projectThree = document.querySelector(".project-three");
+const projectFour = document.querySelector(".project-four");
+
 const urlParams = new URLSearchParams(window.location.search);
+const projectNumber = urlParams.get("project");
 
 // Render project details depending on URL parameters
-const projectName = urlParams.get("project");
-console.log(projectName);
+renderProject(projectNumber);
+
+function renderProject(projectNumber) {
+  if (projectOne.className == projectNumber) {
+    projectOne.removeAttribute("hidden");
+  } else if (projectTwo.className == projectNumber) {
+    projectTwo.removeAttribute("hidden");
+  } else if (projectThree.className == projectNumber) {
+    projectThree.removeAttribute("hidden");
+  } else {
+    projectFour.removeAttribute("hidden");
+  }
+}
