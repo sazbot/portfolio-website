923c637258d7b78e7535d579975237efff4609df
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-09-02 13:49:20 +0100

    refactor renderSlider function

--- render-project.js
+++ render-project.js
@@ -1,46 +1,36 @@
-const projectOne = document.querySelector(".project-one");
-const projectTwo = document.querySelector(".project-two");
-const projectThree = document.querySelector(".project-three");
-const projectFour = document.querySelector(".project-four");
+const projectList = document.querySelectorAll("[data-project]");
 const leftSliderLink = document.querySelector(".left-slider");
 const rightSliderLink = document.querySelector(".right-slider");
 const leftSliderText = document.querySelector(".left-slider-title");
 const rightSliderText = document.querySelector(".right-slider-title");
 
-const urlParams = new URLSearchParams(window.location.search);
-const projectNumber = urlParams.get("project");
-
 const projectNames = ["Manage", "Bookmark", "Insure", "Fylo"];
+
+const urlParams = new URLSearchParams(window.location.search);
+const projectNumber = parseInt(urlParams.get("project"));
 
 // Render project details depending on URL parameters
 renderProject(projectNumber);
-
-// NOTE: REPLACE WITH DATA ATTRIBUTES (i.e. data-project="1") and projectNames.length
+renderSlider(projectNumber);
 
 function renderProject(projectNumber) {
-  if (projectOne.className == projectNumber) {
-    leftSliderText.innerText = projectNames[3];
-    rightSliderText.innerText = projectNames[1];
-    leftSliderLink.href = "./project.html?project=project-four";
-    rightSliderLink.href = "./project.html?project=project-two";
-    projectOne.removeAttribute("hidden");
-  } else if (projectTwo.className == projectNumber) {
-    leftSliderText.innerText = projectNames[0];
-    rightSliderText.innerText = projectNames[2];
-    leftSliderLink.href = "./project.html?project=project-one";
-    rightSliderLink.href = "./project.html?project=project-three";
-    projectTwo.removeAttribute("hidden");
-  } else if (projectThree.className == projectNumber) {
-    leftSliderText.innerText = projectNames[1];
-    rightSliderText.innerText = projectNames[3];
-    leftSliderLink.href = "./project.html?project=project-two";
-    rightSliderLink.href = "./project.html?project=project-four";
-    projectThree.removeAttribute("hidden");
-  } else {
-    leftSliderText.innerText = projectNames[2];
+  projectList[projectNumber - 1].removeAttribute("hidden");
+}
+
+function renderSlider(projectNumber) {
+  leftSliderText.innerText = projectNames[projectNumber - 2];
+  rightSliderText.innerText = projectNames[projectNumber];
+
+  leftSliderLink.href = `./project.html?project=${projectNumber - 1}`;
+  rightSliderLink.href = `./project.html?project=${projectNumber + 1}`;
+
+  //   render slider details for first and last project
+  const numberProjects = projectNames.length;
+  if (projectNumber === 1) {
+    leftSliderText.innerText = projectNames[numberProjects - 1];
+    leftSliderLink.href = `./project.html?project=${numberProjects}`;
+  } else if (projectNumber === numberProjects) {
     rightSliderText.innerText = projectNames[0];
-    leftSliderLink.href = "./project.html?project=project-three";
-    rightSliderLink.href = "./project.html?project=project-one";
-    projectFour.removeAttribute("hidden");
+    rightSliderLink.href = `./project.html?project=1`;
   }
 }
