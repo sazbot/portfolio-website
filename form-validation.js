bef0a5e94e00c9c984e17194ff398a7ee17e992b
Sarah Fielding Smith <s.fieldingsmith@gmail.com>
2022-09-03 15:34:19 +0100

    add custom form validation

--- form-validation.js
+++ form-validation.js
@@ -1,34 +1,79 @@
 const form = document.querySelector("form");
-const contactName = document.querySelector("#name");
-const email = document.querySelector("#email");
-const message = document.querySelector("#message");
+const submitButton = document.querySelector(".submit-btn");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  checkInputs();
-});
+suppressValidationUI(form);
 
-function checkInputs() {
-  const contactNameValue = contactName.value.trim();
-  const emailValue = email.value.trim();
-  const messageValue = message.value.trim();
+relocateErrorMessages();
 
-  if (!contactNameValue) {
-    setErrorFor(contactName, "This field is required");
-  }
+replaceValidationUI();
+
+hideErrorMessages();
+
+function suppressValidationUI(form) {
+  // Suppress the default bubbles
+  form.addEventListener(
+    "invalid",
+    (e) => {
+      e.preventDefault();
+    },
+    true
+  );
+
+  // Support Safari, iOS Safari and Android
+  form.addEventListener("submit", (e) => {
+    if (!form.checkValidity()) {
+      e.preventDefault();
+    }
+  });
 }
 
-function setErrorFor(input, message) {
-  const inputGroup = input.parentElement;
-  const errorMessage = inputGroup.querySelector(".error-msg");
+function relocateErrorMessages() {
+  submitButton.addEventListener("click", () => {
+    let invalidFields = form.querySelectorAll(":invalid"),
+      errorMessages = form.querySelectorAll(".error-msg"),
+      parent;
 
-  //   add error message
-  errorMessage.innerText = message;
+    // Remove any existing messages
+    for (let i = 0; i < errorMessages.length; i++) {
+      errorMessages[i].parentNode.removeChild(errorMessages[i]);
+      errorMessages[i].classList.remove("error");
+    }
 
-  //   add error class
-  inputGroup.classList.add("error");
+    //   Insert default error message
+    for (let i = 0; i < invalidFields.length; i++) {
+      let parent = invalidFields[i].parentNode;
+      parent.insertAdjacentHTML(
+        "beforeend",
+        "<div class='error-msg'>" +
+          invalidFields[i].validationMessage +
+          "</div>"
+      );
+      parent.classList.add("error");
+    }
+
+    // If there are errors, give focus to the first invalid field
+    if (invalidFields.length > 0) {
+      invalidFields[0].focus();
+    }
+  });
 }
-function setSuccessFor(input, message) {
-  const inputGroup = input.parentElement;
-  inputGroup.classList.remove("error");
+
+function replaceValidationUI() {
+  let forms = document.querySelectorAll("form");
+  forms.forEach((form) => {
+    suppressValidationUI(form);
+  });
+}
+
+function hideErrorMessages() {
+  // Hide error message when input/textarea field clicked
+
+  document.addEventListener("click", (e) => {
+    const selectedField = e.target;
+
+    if (selectedField.classList.value === "text-input") {
+      selectedField.parentNode.classList.remove("error");
+      selectedField.parentNode.lastChild.remove();
+    }
+  });
 }
